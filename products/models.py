b0dcd566db6bf88d5605e3fba0057c265fcb0f95
maoyed <qwermoaid17@gmail.com>
2026-08-11 01:30:39 +0200

    Lesson 23: Practice some methods and add them to a models file such as True, blank, deffualt, choisec

--- products/models.py
+++ products/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 class Product(models.Model):
 
-    name= models.CharField(max_length=100)
-    content= models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    x= [
+        ('phone', 'phone'),
+        ('computer', 'computer'),
+    ]
+
+    name= models.CharField(max_length=100, default='Product Name')
+    content= models.TextField(null=True, blank=True, verbose_name='description')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    image = models.ImageField(upload_to='photos/%y/%m/%d', verbose_name='photo', default= 'photos/26/08/09/9.png')
     condition = models.BooleanField(default=True)
+    category= models.CharField(max_length=50, null=True, blank=True, choices=x)
 
     def __str__(self):
 
@@ -14,5 +20,5 @@ class Product(models.Model):
 
     class Meta:
         
-        verbose_name = 'good'
-        ordering = ['-name']
+        verbose_name = 'product'
+        ordering = ['name']
