05dd9c5e7bea34abce5b7e472babd5f594b82b64
maoyed <qwermoaid17@gmail.com>
2026-08-11 21:33:53 +0200

    Lesson 25: Practice Relationships

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Female, Male, User, Product, username, Videw
 
-# Register your models here.
+admin.site.register(Female)
+admin.site.register(Male)
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(username)
+admin.site.register(Videw)
--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,62 @@
 from django.db import models
+
+class Female(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return self.name
+    
+
+    
+class Male(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    girls = models.OneToOneField(Female, on_delete=models.CASCADE, )
+
+    def __str__(self):
+
+        return self.name
+
+    
+class Product(models.Model):
+
+    name_products = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return self.name_products
+    
+class User(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    prod = models.ForeignKey(Product, max_length=50, on_delete=models.CASCADE)
+
+    def __str__(self):
+
+        return self.name
+
+class Videw(models.Model):
+
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return self.title
+    
+class username(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    vid = models.ManyToManyField(Videw, max_length=50,)
+
+    def __str__(self):
+
+        return self.name
+
+
+
+
