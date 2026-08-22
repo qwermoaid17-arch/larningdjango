18543c25540ae48b425cb923131aa2470ffa7651
maoyed <qwermoaid17@gmail.com>
2026-08-23 00:24:12 +0200

    Lesson 38 (the final lesson) teaches you how to add features to the admin panel such as search, filtering, editing, and other features.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Product, Test
 
-admin.site.register(Product)
+
+
+class Products_Admin(admin.ModelAdmin):
+
+    list_display = ['name', 'price', 'condition', 'category']
+    list_display_links = ['name']
+    list_editable = ['price', 'category', 'condition']
+    search_fields = ['name']
+    list_filter = ['category', 'price']
+    fields = ['name', 'price', 'condition']
+
+
+
+admin.site.register(Product, Products_Admin)
 admin.site.register(Test)
